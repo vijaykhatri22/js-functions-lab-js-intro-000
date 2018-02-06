c1da94abc1a99a118469e830b5ca86dfa16e47c3
vijay khatri <vijaykhatri22@gmail.com>
2018-02-06 11:55:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name){
   return  `Happy ${holiday}, ${name}!`;
 }
 
-function holidatCountdown(holiday, days){
+function holidayCountdown(holiday, days){
   return  `It's ${days} days until ${holiday}!`; 
 }
